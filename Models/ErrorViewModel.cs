809df838fcc5b779f0cb75ca7562ef907aca8a73
JorgeDLG <idroj07@gmail.com>
2020-04-09 01:31:30 +0200

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcDotCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
